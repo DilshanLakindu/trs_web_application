15ef61a8d592254412a696110f694203317398e7
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2023-10-11 13:04:19 +0530

    Initial Commit

---
+++ src/screens/UserManagement.js
@@ -0,0 +1,11 @@
+import UserManagement from "../components/UserManagement";
+
+const userManagement = () => {
+  return (
+    <div className="container p-lg-5">
+      <UserManagement />
+    </div>
+  );
+};
+
+export default userManagement;
